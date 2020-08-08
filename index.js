72496f9a1f332ba4dc457c7141375dbe22855992
iamchiranjitdey <chiranjitdey2000@gmail.com>
2020-08-08 22:00:59 +0530

    create element done

--- index.js
+++ index.js
@@ -11,5 +11,22 @@ function barchart(){
         parseInt(
             100* (Math.random() + 1), 10
         )
-    ),
+    );
+    function chartGenerate(data) {
+        const chartElems = [];
+
+        const create = (d) => {
+            d.forEach((entry, index) => {
+                const bar = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+                bar.setAttribute('x', index * (600 / data.length));
+                bar.setAttribute('y',600-y);
+                bar.setAttribute('width',`${600/ data.length}px`);
+                bar.setAttribute('height', `${0}px`);
+                bar.setAttribute('style', 'transition : 0.5s all');
+                svg.appendChild(bar);
+                chartElems.push(bar);
+            });
+        };
+    }
+            
 };
